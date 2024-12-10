d2b1250c6cda6af4096c7fcc3e04707d6eb1459f
Brian <briane0717@gmail.com>
2024-12-10 18:36:18 -0500

    added all functionality and information

---
+++ ALAB-320H.1.1/src/components/EmployeePage.jsx
@@ -0,0 +1,21 @@
+function EmployeePage() {
+  return (
+    <div className="employee">
+      <div>
+        <header>
+          <h2>Employee</h2>
+        </header>
+        <div className="name">Julie Taylor</div> <br />
+        <div className="title">VP of Marketing</div> <br />
+        <div className="office">Call Office</div> <br />
+        <div className="mobile">Call Mobile</div>
+        <br />
+        <div className="text">SMS</div> <br />
+        <div className="email">Email</div> <br />
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default EmployeePage;
--- ALAB-320H.1.1/src/components/Homepage.jsx
+++ ALAB-320H.1.1/src/components/Homepage.jsx
@@ -1,13 +1,16 @@
 import Header from "./Header";
 import SearchBar from "./SearchBar";
 import EmployeeList from "./EmployeeList";
+import EmployeePage from "./EmployeePage";
 
 function Homepage() {
   return (
     <div>
       <Header />
       <SearchBar />
+      <br />
       <EmployeeList />
+      <EmployeePage />
     </div>
   );
 }
